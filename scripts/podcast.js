45f257a7e1788d4d62efb9ef35bd743d274f60f2
Anowond <baroux.maxime2@gmail.com>
2024-01-06 11:04:05 +0100

    on laisse tomber l'idée du TTS et on continue le TP

--- scripts/podcast.js
+++ scripts/podcast.js
@@ -1,49 +1,35 @@
 import podcasts from "./podcasts.js";
-import { callAPI } from "./api.js";
-
-
-let button = document.getElementById("button")
 
 // Décomposition de l'objbet podcast
-podcasts.map( async podcast => {
- 
+podcasts.map(podcast => {
+
     // Pour chaque élément, créer une balise dans le fichier HTML
-    
+
     // Pour la date
-    let date = document.createElement("h2")
+    let date = document.createElement("i")
     date.classList.add("date")
     date.textContent = podcast.date
     document.body.appendChild(date)
-    
+
     // Pour le nom
-    let name = document.createElement("p")
+    let name = document.createElement("h2")
     name.classList.add("name")
     name.textContent = podcast.name
     document.body.appendChild(name)
-    
+
     // Pour le contenu
     let content = document.createElement("p")
     content.classList.add("content")
     content.textContent = podcast.content
     document.body.appendChild(content)
-    
-    
-    // On intégre un lecteur audio qui lira le contenu de la variable speach
 
-        let response = await callAPI(content.textContent)
-        console.log("response : ", response)
-    
-
-    // Response n'est pas exploitable en l'état, on doit passer par la création d'un blob
-    let blob = new Blob([response.data], {type: 'audio/mpeg'})
-    let url = URL.createObjectURL(blob)
-    console.log(url)
-    
+    // Pour l'audio
     let audio = document.createElement("audio")
+    audio.preload = true
     audio.controls = true
-    audio.src = url
+    audio.src = podcast.url
     document.body.appendChild(audio)
-    
+
     console.log(audio)
-    
+
 })
