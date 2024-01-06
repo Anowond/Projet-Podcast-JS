df67c25f04ec58f315296f2b241a9323855547dd
Anowond <baroux.maxime2@gmail.com>
2024-01-06 13:49:12 +0100

    TP fait en front pour le fun

--- scripts/podcast.js
+++ scripts/podcast.js
@@ -1,35 +1,53 @@
-import podcasts from "./podcasts.js";
+import { getStorage } from "./localstorage.js"
 
-// Décomposition de l'objbet podcast
-podcasts.map(podcast => {
-
-    // Pour chaque élément, créer une balise dans le fichier HTML
-
-    // Pour la date
-    let date = document.createElement("i")
-    date.classList.add("date")
-    date.textContent = podcast.date
-    document.body.appendChild(date)
-
-    // Pour le nom
-    let name = document.createElement("h2")
-    name.classList.add("name")
-    name.textContent = podcast.name
-    document.body.appendChild(name)
-
-    // Pour le contenu
-    let content = document.createElement("p")
-    content.classList.add("content")
-    content.textContent = podcast.content
-    document.body.appendChild(content)
-
-    // Pour l'audio
-    let audio = document.createElement("audio")
-    audio.preload = true
-    audio.controls = true
-    audio.src = podcast.url
-    document.body.appendChild(audio)
-
-    console.log(audio)
-
+// Bouton de retour à l'index
+let btnRetour = document.getElementById("return")
+btnRetour.addEventListener("click", () => {
+    location.href = "../index.html"
 })
+
+// Récupération de l'outerHTML stocké en localStorage
+let storage = getStorage()
+
+// Création des éléments HTML
+
+// Création de la div conteneur
+let divPodcast = document.createElement("div")
+divPodcast.classList.add("divPodcast")
+document.body.appendChild(divPodcast)
+
+// Pour la date
+let date = document.createElement("i")
+date.classList.add("date")
+date.textContent = storage.date
+divPodcast.appendChild(date)
+
+// Pour le nom
+let name = document.createElement("h2")
+name.classList.add("name")
+name.textContent = storage.name
+divPodcast.appendChild(name)
+
+// Pour le contenu
+let content = document.createElement("p")
+content.classList.add("content")
+content.textContent = storage.content
+divPodcast.appendChild(content)
+
+// Pour l'audio
+let divAudio = document.createElement("div")
+divAudio.classList.add("divAudio")
+divPodcast.appendChild(divAudio)
+
+let audio = document.createElement("audio")
+audio.classList.add("audio")
+audio.controls = true
+audio.preload = true
+audio.src = storage.url
+divAudio.appendChild(audio)
+
+
+
+
+
+
